cdd4aea33d25930142a72463cef12dc45f0eaded
karyliz <karenlizettecabrera@gmail.com>
2017-11-02 14:59:23 -0300

    se añadio una nueva clase.se añadio un hola mundo

--- Cualquiera/src/cualquiera/Cualquiera.java
+++ Cualquiera/src/cualquiera/Cualquiera.java
@@ -4,6 +4,7 @@ public class Cualquiera {
 
     public static void main(String[] args) {
         System.out.println("holaxd");
+        System.out.println("hola mundo");
     }
     
 }
---
+++ Cualquiera/src/cualquiera/clase2.java
@@ -0,0 +1,15 @@
+package cualquiera;
+
+public class clase2 {
+    private String nombre;
+    private String apellido;
+    private int edad;
+
+    public clase2(String nombre, String apellido, int edad) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+    }
+    
+    
+}
